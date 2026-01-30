21cf4b53dcea94f2ee94749da93ec623964b0110
hmpmanish <hmpmanish@gmail.com>
2026-01-30 22:20:39 +0530

    experiment_3_distance.py

---
+++ experiment_4_positive_negative_zero.py
@@ -0,0 +1,11 @@
+# hmpmanish
+# Experiment 4: Check whether a number is Positive, Negative or Zero
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
