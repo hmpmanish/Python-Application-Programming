6ebc45efc17a4357b7e19a6c54487b587c366742
hmpmanish <hmpmanish@gmail.com>
2026-02-11 22:52:45 +0530

    Auto commit by Python script

---
+++ Lists/exp-1_list_negative.py
@@ -0,0 +1,11 @@
+# Create a list
+l = ["a", "b", "c", "manish", "hmp", "manish"]
+
+# Print last element of the list
+print(l[-1])   # -1 means last element → manish
+
+# Print second last element
+print(l[-2])   # -2 means second last element → hmp
+
+# Print third last element
+print(l[-3])   # -3 means third last element → manish
