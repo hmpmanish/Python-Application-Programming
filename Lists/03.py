f705dd04498e443be7cccf12b222ac4105c0bc76
hmpmanish <hmpmanish@gmail.com>
2026-02-11 23:12:22 +0530

    Auto commit by Python script

---
+++ Lists/03.py
@@ -0,0 +1,7 @@
+# Create a nested list
+l = ["hmp", [8, 4, 6], ["a"]]
+
+# Print slicing from index 2 to 5
+# Syntax: list[start:end]
+# Start index included, end index excluded
+print(l[1:])   # This will print elements from index 2 onward
