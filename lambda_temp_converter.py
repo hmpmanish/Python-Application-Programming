0506cb228f4df887688a2100fbdcd09f131ca688
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-03-25 09:46:09 +0530

    Update and rename temp_converter.py to lambda_temp_converter.py

---
+++ lambda_temp_converter.py
@@ -0,0 +1,3 @@
+temp = lambda a: (a * 9/5) + 32
+n = int(input("Enter temperature in Celsius: "))
+print("Temp in Fahrenheit:", temp(n))
--- temp_converter.py
+++
@@ -1,2 +0,0 @@
-temp = lambda a: (a * 9/5) + 32
-print(temp(19), "fahrenheit")
