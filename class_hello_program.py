b65e8c256408a04c6f7419f83409a0b0f2b4f979
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-04-01 09:58:40 +0530

    Add class to print greeting messages
    Implement a class to print a greeting message with a name.

---
+++ class_hello_program.py
@@ -0,0 +1,11 @@
+#wap to print hello with your name 
+# create class
+class csb:
+    def message(self, name):
+        print("Hii", name)
+
+# create object
+obj1 = csb()
+obj2 = csb()
+obj1.message("obj1")
+obj2.message("obj2")
