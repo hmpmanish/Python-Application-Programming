26a27505dfcaa5adb3736cc615c72e128b44e049
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-04-08 10:12:44 +0530

    Implement file write and read operations
    Added functionality to write data to two files and read from one.

--- fl.py
+++ fl.py
@@ -26,3 +26,43 @@ print(f.readline())
 # Close the file after reading
 f.close()
 
+
+# WAP (Write a Program) to open a file and write data into it
+
+# Open the file in write mode ("w")
+# If the file does not exist, it will be created
+# If it exists, its previous content will be erased
+f = open(r"C:\Users\blk3-308\hmpmanish1.txt", "w")
+
+# Write data into the file
+# write() returns the number of characters written
+print(f.write("Hello Manish 2"))
+
+# Close the file after writing
+f.close()
+
+
+
+
+# WAP (Write a Program) to open a file and write a list, then read from another file
+
+# Open file 'hmpmanish.txt' in write mode ("w")
+# If the file does not exist, it will be created
+f = open(r"C:\Users\blk3-308\hmpmanish.txt" ,"w")
+
+# Create a list of names
+L = ["Manish", "yash", "vikky", "swastik", "naman"]
+
+# Write the list to the file
+# Note: writelines() writes items as-is, without adding newlines
+(f.writelines(L))
+
+# Close the file after writing
+f.close()
+
+# Open another file 'hmpmanish1.txt' in read mode ("r")
+# This file must exist, otherwise it will cause FileNotFoundError
+f = open(r"C:\Users\blk3-308\hmpmanish1.txt" ,"r")
+
+# Read and print only the first line of the file
+print(f.readline())
