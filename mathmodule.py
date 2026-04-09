ee843d2669eaff848d8cfc1703be3c2c5562113d
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-04-09 11:08:55 +0530

    Add various math operations to mathmodule.py

---
+++ mathmodule.py
@@ -0,0 +1,52 @@
+import math
+print(math.sqrt(25))
+
+
+import math
+print(math.pi)
+
+
+import math
+print(math.e)
+
+
+import math
+print(math.pow(2,4))
+
+
+import math
+print(math.radians(30))
+
+
+import math
+print(math.degrees(math.pi)
+
+
+      import math
+print(math.sin(30))
+
+
+import math
+print(math.log(10))
+
+
+
+import math
+print(math.log10(10))
+
+
+
+import math
+print(math.exp(10))
+
+
+import math
+print(math.ceil(4.5867))
+
+
+import math
+print(math.floor(4.5867))
+
+
+import math
+print(math.trunc(12.32))
