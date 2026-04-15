16d92b9485d3dce638325ba9be20f39b983ea14e
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-04-15 10:29:41 +0530

    Update System_ID values in pandas.py

--- pandas.py
+++ pandas.py
@@ -1,10 +1,43 @@
+# -------- DataFrame example --------
+
+
+
+
 import pandas as pd
 
 mydataset = {
     "Name": ["Manish", "Yash", "Atul"],
-    "System_ID": [20250666283, 202564545, 6569544]
+    "System_ID": [2025066457453, 202564545, 6569544]
 }
 
 myvar = pd.DataFrame(mydataset)
+
+print(myvar)
+
+
+
+# -------- Series example --------
+
+
+
+import pandas as pd
+
+data = ["Manish", "Yash", "Atul"]
+
+myvar = pd.Series(data)
+
+print(myvar)
+
+
+
+# -------- DataFrame from list --------
+
+
+
+import pandas as pd
+
+data = ["Manish", "Yash", "Atul"]
+
+myvar = pd.DataFrame(data)
 
 print(myvar)
