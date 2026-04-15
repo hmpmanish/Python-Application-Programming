e47b75678d36b06f7605159a72be0afb9924d3f6
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-04-15 09:28:56 +0530

    Fix indentation and remove duplicate code

--- matplotlib.py
+++ matplotlib.py
@@ -29,3 +29,14 @@ ypoints = np.array([3,8 ,1 , 10])
 
 plt.plot(xpoints, ypoints )
 plt.show()
+
+
+# defult x points 
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3,8 ,1 , 10])
+
+plt.plot(xpoints, ypoints )
+plt.show()
+
