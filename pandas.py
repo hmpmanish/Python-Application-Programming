9802e64603be47499669448491332ab9750fb16d
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-04-15 10:41:34 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -58,3 +58,24 @@ index_names = ["sparrow", "fox", "spider", "snake"]
 myvar = pd.DataFrame(data, index=index_names)
 
 print(myvar)
+
+
+
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\blk3-308\Desktop\data.csv")
+
+print(df.to_string())
+
+print(df.head())
+print(df.tail())
+
+df.info()
+
+
+Name,Age,City,Marks
+Manish,20,Delhi,85
+Yash,21,Mumbai,90
+Atul,19,Pune,78
+Ravi,22,Delhi,88
+Neha,20,Bangalore,92
