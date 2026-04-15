fab06facc19c4537a616556ae305fd121ad8ce2f
Manish Pandey <156882775+hmpmanish@users.noreply.github.com>
2026-04-15 10:33:00 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -41,3 +41,19 @@ data = ["Manish", "Yash", "Atul"]
 myvar = pd.DataFrame(data)
 
 print(myvar)
+
+
+import numpy as np
+import pandas as pd
+
+data = {
+    "num_legs": [2, 4, 8, 0],
+    "num_wings": [2, 0, 0, 0],
+    "num_specimens": [8, 2, 5, 6]
+}
+
+index_names = ["sparrow", "fox", "spider", "snake"]
+
+myvar = pd.DataFrame(data, index=index_names)
+
+print(myvar)
