2985a6c42aca84610d75924072653945ff6c46f6
hmpmanish <hmpmanish@gmail.com>
2026-04-21 14:45:28 +0530

    Refactor file handling in fl.py and add context manager for better resource management

---
+++ 2104.py
@@ -0,0 +1,8 @@
+class Car:
+    color = "red"
+    brand = "BMW"
+car1 = Car()
+print(car1.color)
+print(car1.brand)
+
+car
--- fl.py
+++ fl.py
@@ -1,17 +1,32 @@
-f = open(r"C:\Users\Manish Pandey\Desktop\sample.txt", "w")
+# Writing to a file
+f = open("sample.txt", "w")
 f.write("Hello, File!\n")
 f.write("Second line\n")
+f.writelines(["Third line\n", "Fourth line\n"])
 f.close()
-
-
-f = open(r"C:\Users\Manish Pandey\Desktop\sample.txt", "r")
-content = f.read()
+# Reading a file
+f = open("sample.txt", "r")
+print(f.read()) # Read entire file
 f.close()
-print(content)
-
-f = open(r"C:\Users\Manish Pandey\Desktop\sample.txt", "a")
-f.write("Appended line\n")
+f = open("sample.txt", "r")
+print(f.readline()) # Read one line
 f.close()
-
-
+f = open("sample.txt", "r")
+print(f.readlines()) # Read all lines as list
+f.close()
+# Best practice: using 'with' statement (context manager)
+with open("sample.txt", "r") as f:
+for line in f:
+print(line.strip()) # Read line by line
+# Appending to a file
 with open("sample.txt", "a") as f:
+f.write("Appended line\n")
+# Count words in a file
+with open("sample.txt", "r") as f:
+content = f.read()
+words = content.split()
+print("Word count:", len(words))
+# Copy file content
+with open("sample.txt", "r") as src:
+with open("copy.txt", "w") as dst:
+dst.write(src.read())
